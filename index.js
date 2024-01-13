41fc25e70c5236d64e6d93a06bd2627448fa3146
dayana <dayana.romero88@gmail.com>
2024-01-13 08:31:14 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const simpsonsRouter = require('./src/api/routes/simpson.routes');
+const ciudadRouter = require('./src/api/routes/ciudad.routes');
+const userRouter = require('./src/api/routes/user.routes');
+dotenv.config();
+
+const {connect} = require('./src/utils/db');
+const { isAuth } = require('./src/middlewares/auth');
+const port = process.env.PORT || 7000;
+
+const app = express();
+connect();
+
+app.use(express.json());
+
+app.use('/simpsons', simpsonsRouter);
+app.use('/ciudad', [isAuth], ciudadRouter);
+app.use('/user', userRouter);
+
+app.listen(port, () => console.log('listening on port ' + port));
